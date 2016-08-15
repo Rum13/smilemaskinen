f5f48d2f727df7ab1a51cb3133e8df0bfa72740c
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2016-08-15 17:24:58 +0200

    Lots of beautification

--- smilemaskinen.ino
+++ smilemaskinen.ino
@@ -1,10 +1,13 @@
-#define potPin A1
+#define ldrPin A1
 #define servoPin 11
 #define transistorPin 12
 #define loMapFrom 200
 #define hiMapFrom 700
 #define loMapTo 50
 #define hiMapTo 120
+#define moveThreshold 10
+#define servoDelay 250
+#define avgCounter 20
 
 #include <LowPower.h>
 #include <Servo.h>
@@ -12,27 +15,43 @@
 Servo myservo;
 
 
-int potVal;    // variable to read the value from the analog pin
+unsigned long lightVal;    // variable to read the value from the analog pin
 int oldVal = 200;
 
 void setup() {
-  pinMode(transistorPin, OUTPUT);
-  myservo.attach(servoPin);
+    pinMode(ldrPin, INPUT);
+    pinMode(transistorPin, OUTPUT);
+    myservo.attach(servoPin);
 }
 
 void loop() {
-  potVal = analogRead(potPin);
-  int servoVal = map(potVal, loMapFrom, hiMapFrom, loMapTo, hiMapTo);
+    // reads the value from the ldr avgCounter-times and calculates the average
+    for(int i = 0; i < avgCounter; i++){
+        lightVal += (unsigned long) analogRead(ldrPin);
+    }
+    lightVal = lightVal / avgCounter;
 
-  if(abs(oldVal - servoVal)>=10){
-    digitalWrite(transistorPin, HIGH);
-    myservo.write(servoVal);                  // sets the servo position according to the scaled value
-    
-    oldVal = servoVal;
-    
-    delay(250);                           // waits for the servo to get there
-    digitalWrite(12,LOW);
-  }
+    // constrains the light value to be inside the range we're mapping from
+    lightVal = constrain(lightVal, loMapFrom, hiMapFrom);
+    // scales the value from the ldr to a value for the servo
+    int servoVal = map(lightVal, loMapFrom, hiMapFrom, loMapTo, hiMapTo);
 
-  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+    if(abs(oldVal - servoVal) >= moveThreshold){
+        // turning on the power to the servo by turning on the transistor
+        digitalWrite(transistorPin, HIGH);
+
+        // sets the servo position according to the scaled value
+        myservo.write(servoVal);
+
+
+        // waits for the servo to get there
+        delay(servoDelay);
+        // turns off the power to the servo by turning the transistor off
+        digitalWrite(transistorPin, LOW);
+
+        // saves the new servo value
+        oldVal = servoVal;
+    }
+
+    LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
 }
